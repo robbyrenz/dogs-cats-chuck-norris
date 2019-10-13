34d303e7730952ca9e430b4bfce49473002dd905
Robby Renz <nxgrenz@gmail.com>
2019-10-13 16:38:19 -0500

    add code that prints out chuck norris facts

--- index.js
+++ index.js
@@ -2,4 +2,30 @@ document.addEventListener("DOMContentLoaded", start())
 
 function start() {
     console.log("Hello World!")
+    document.getElementById("dog").addEventListener("click", dogImage)
+    document.getElementById("cat").addEventListener("click", catImage)
+}
+
+function dogImage() {
+    console.log("You clicked on the dog button!")
+    // fetch an image of a dog
+    getFacts()
+}
+
+function catImage() {
+    console.log("You clicked on the cat button!")
+    // fetch an image of a cat
+    getFacts()
+}
+
+function getFacts() {
+    enterText("loading...")
+    fetch("https://api.chucknorris.io/jokes/random")
+        .then(res => res.json())
+        .then(res => enterText(res.value))
+
+}
+
+function enterText(text) {
+    document.getElementById("fact").innerHTML = text
 }
